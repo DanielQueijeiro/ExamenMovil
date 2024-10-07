1edf1f7e7c93b9db8f54c631d90623b95adfbe41
DanielQueijeiro <dany.queijeiro@hotmail.com>
2024-10-07 08:57:19 -0600

    commit test para probar repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,23 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    //Data binding
+    implementation ("androidx.databinding:databinding-runtime:7.1.2")
+    // ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    // LiveData
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation ("jp.wasabeef:glide-transformations:4.3.0")
+    //Activity
+    implementation ("androidx.activity:activity-ktx:1.5.0")
+    // OkHttp
+    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
 }
